b073388df21fa21e17e722fda17ea8177a908c1e
yanglie <viking6413@sina.com>
2019-05-16 18:13:08 +0800

    data

---
+++ data/conf/route.php
@@ -0,0 +1,71 @@
+<?php	return array (
+  '在读数据集/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+      'cid' => '\\d+',
+    ),
+  ),
+  '待读书籍/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=2',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+      'cid' => '\\d+',
+    ),
+  ),
+  '已读书籍/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=3',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+      'cid' => '\\d+',
+    ),
+  ),
+  '在读数据集' => 
+  array (
+    0 => 'portal/List/index?id=1',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+  '待读书籍' => 
+  array (
+    0 => 'portal/List/index?id=2',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+  '已读书籍' => 
+  array (
+    0 => 'portal/List/index?id=3',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\\d+',
+    ),
+  ),
+);
