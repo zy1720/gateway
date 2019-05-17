8dd2b7b2d0977efee5d8bee08baefe2345f6a037
yanglie <viking6413@sina.com>
2019-05-17 17:30:49 +0800

    sousuo

--- app/config.php
+++ app/config.php
@@ -137,7 +137,7 @@ $configs = [
     // 异常页面的模板文件
     'exception_tmpl'         => THINK_PATH . 'tpl' . DS . 'think_exception.tpl',
     // 错误显示信息,非调试模式有效
-    'error_message'          => '123页面错误！请稍后再试～',
+    'error_message'          => '页面错误！请稍后再试～',
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
--- simplewind/thinkphp/convention.php
+++ simplewind/thinkphp/convention.php
@@ -152,7 +152,7 @@ return [
     'exception_tmpl'         => THINK_PATH . 'tpl' . DS . 'think_exception.tpl',
 
     // 错误显示信息,非调试模式有效
-    'error_message'          => '123123页面错误！请稍后再试～',
+    'error_message'          => '页面错误！请稍后再试～',
     // 显示错误信息
     'show_error_msg'         => false,
     // 异常处理handle类 留空使用 \think\exception\Handle
